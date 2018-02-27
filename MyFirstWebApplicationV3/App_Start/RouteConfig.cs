222d7d25c2ad0cdc76c5f0c1cb9870ef5175e911
GuitarmanEG140 <stephen@supplyamericaonline.com>
2018-02-27 16:55:52 -0600

    Commit Changes To Branch Assignment4.1

--- MyFirstWebApplicationV3/App_Start/RouteConfig.cs
+++ MyFirstWebApplicationV3/App_Start/RouteConfig.cs
@@ -12,6 +12,11 @@ namespace MyFirstWebApplicationV3
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "NewRoute",
+                url: "{controller}/{action}",
+                defaults: new { controller = "CustomRoute", action = "AboutThisPage" }
+            );
 
             routes.MapRoute(
                 name: "Default",
